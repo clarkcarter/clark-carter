1bdc3f1f8044c395b9b81f2a170d94449650e666
Clark Carter <clark@clarkcarter.com>
2017-10-06 20:37:00 +1100

    add images for gallery

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import { Gallery, Image, Overlay } from 'react-stylish-gallery'
 
 const imageGallery = [
-  "",
-  "",
-  "",
-  
+  "https://github.com/clarkcarter/clark-carter/blob/master/src/images/160129-4x5-01-A.jpg?raw=true",
+  "https://github.com/clarkcarter/clark-carter/blob/master/src/images/160227-4x5-01-E.jpg?raw=true",
+  "https://github.com/clarkcarter/clark-carter/blob/master/src/images/160227-4x5-02-F.jpg?raw=true",
 ]
 
 const IndexPage = () => (
   <div>
-    {}
+    <Gallery withModal>
+      <Image src={imageGallery[0]} alt="Image 1" />
+      <Image src={imageGallery[1]} alt="Image 2" />
+      <Image src={imageGallery[2]} alt="Image 2" />
+    </Gallery>
   </div>
 )
 
