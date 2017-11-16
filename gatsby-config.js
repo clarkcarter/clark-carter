bef1c12c78e980ab0689136a77c7a59a6115ee7f
Clark Carter <clark@clarkcarter.com>
2017-11-17 08:18:59 +1100

    update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Clark Carter`,
+    title: `Clark Carter | Photographer`,
+    description: `Clark Carter is an Australian landscape photographer and lover of wild places, specialising in large and medium format film.`
   },
   pathPrefix: `/`,
 }
